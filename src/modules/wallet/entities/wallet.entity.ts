13c303cad225aef44bd5bfaa2cfdb2ad73731a7d
rgstartup <rgstartup1@gmail.com>
2026-02-03 10:56:25 +0530

    ..

--- src/modules/wallet/entities/wallet.entity.ts
+++ src/modules/wallet/entities/wallet.entity.ts
@@ -9,6 +9,15 @@ import {
 } from 'typeorm';
 import { User } from '../../users/entities/user.entity';
 
+export class ColumnNumericTransformer {
+  to(data: number): number {
+    return data;
+  }
+  from(data: string): number {
+    return parseFloat(data);
+  }
+}
+
 @Entity('wallets')
 export class Wallet {
   @PrimaryGeneratedColumn()
@@ -21,7 +30,13 @@ export class Wallet {
   @Column({ name: 'user_id' })
   userId: number;
 
-  @Column({ type: 'decimal', precision: 10, scale: 2, default: 0 })
+  @Column({
+    type: 'decimal',
+    precision: 10,
+    scale: 2,
+    default: 0,
+    transformer: new ColumnNumericTransformer(),
+  })
   balance: number;
 
   @Column({
@@ -30,6 +45,7 @@ export class Wallet {
     scale: 2,
     default: 0,
     name: 'reserved_balance',
+    transformer: new ColumnNumericTransformer(),
   })
   reservedBalance: number;
 
