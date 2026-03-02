1190d74dc57d95ba6871f9c2f87b160e4735242e
rgstartup <rgstartup1@gmail.com>
2026-03-02 15:57:46 +0530

    ..

--- src/modules/expert/expert.module.ts
+++ src/modules/expert/expert.module.ts
@@ -3,9 +3,22 @@ import { ProfileModule } from './profile/profile.module';
 import { BankAccountsModule } from './bank-accounts/bank-accounts.module';
 import { ExpertEarningsModule } from './earnings/expert-earnings.module';
 import { ExpertDashboardModule } from './dashboard/expert-dashboard.module';
+import { TodosModule } from './todos/todos.module';
 
 @Module({
-  imports: [ProfileModule, BankAccountsModule, ExpertEarningsModule, ExpertDashboardModule],
-  exports: [ProfileModule, BankAccountsModule, ExpertEarningsModule, ExpertDashboardModule]
+  imports: [
+    TodosModule,
+    BankAccountsModule,
+    ExpertEarningsModule,
+    ExpertDashboardModule,
+    ProfileModule,
+  ],
+  exports: [
+    TodosModule,
+    BankAccountsModule,
+    ExpertEarningsModule,
+    ExpertDashboardModule,
+    ProfileModule,
+  ],
 })
 export class ExpertModule { }
