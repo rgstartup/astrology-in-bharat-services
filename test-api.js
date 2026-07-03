a79164361dd91904fd2c555d5d721136287e5815
Ravi Rai <ravirairgstartup@gmail.com>
2026-07-03 12:56:59 +0530

    chore(backend): remove various scratch scripts, test files, archive, and lint reports

--- test-api.js
+++
@@ -1,6 +0,0 @@
-﻿const http = require("http");
-http.get("http://localhost:3000/api/v1/chat/session/e2cd6a27-db56-4c96-aba6-302a2ec96781", (res) => {
-  let data = "";
-  res.on("data", (chunk) => data += chunk);
-  res.on("end", () => console.log(data));
-}).on("error", (err) => console.log("Error: " + err.message));
